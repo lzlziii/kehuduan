b530b16094ff06e730c34bf4670540e3be0238b1
lz <2407809684@qq.com>
2021-12-27 10:44:52 +0800

    sendAuto成功

--- app/src/main/java/xx/yy/testhttp/MainActivity.java
+++ app/src/main/java/xx/yy/testhttp/MainActivity.java
@@ -1,27 +1,10 @@
 package xx.yy.testhttp;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
-import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,11 +26,19 @@ public class MainActivity extends AppCompatActivity {
       }
     }.start());
 
-
     findViewById(R.id.sendBig).setOnClickListener(v -> new Thread() {
       public void run() {
         try {
           Utils.sendObject("big", "big", new BigInteger("181237128738172831273817238123971631"));
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    }.start());
+    findViewById(R.id.sendAuto).setOnClickListener(v -> new Thread() {
+      public void run() {
+        try {
+          Utils.sendAuto("auto", 123L);
         } catch (Exception e) {
           e.printStackTrace();
         }
